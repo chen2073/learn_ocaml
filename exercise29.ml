e0f7fb23ee760b0a0f9c61acc7347eda6c22d429
James Chen <chenhao540@gmail.com>
2024-06-10 10:49:16 -0700

    more exercises

--- exercise29.ml
+++ exercise29.ml
@@ -27,4 +27,32 @@ let _ = if 2 > 1 then 42 else 7
 
 (* ex: double fun *)
 let double x = x * 2
-let _ = double 7 |> fun x -> x = 10 |> fun x -> assert x
+let _ = double 7 |> fun x -> x = 10
+
+(* ex: more fun *)
+let cube_float n = pow n 3
+let get_sign n = 
+  if n = 0 then 0
+  else if n < 0 then -1
+  else 1
+let get_area r = pow r 2 *. 3.14
+let test_get_area r true_area = assert ((get_area r) = true_area)
+let _ = test_get_area 5. 78.5
+(* let _ = test_get_area 5.2356745 86.0745826556 *)
+
+(* ex: rms *)
+let rms x y = 
+  let x_square = x *. x in 
+  let y_square = y *. y in 
+  let sum_suqare = x_square +. y_square in 
+  let half_sum_square = sum_suqare /. 2. in 
+  let sqrt_half_sum_square = sqrt half_sum_square in
+  sqrt_half_sum_square
+
+(* ex: date fun *)
+let date_fun (d: int) (m: string) : bool = 
+  match m with
+    | "Feb" -> 0 < d && d <= 28
+    | "Jan" | "Mar" | "May" | "Jul" | "Aug" | "Oct" | "Dec" -> 0 < d && d <= 31
+    | "Apr" | "Jun" | "Sept" | "Nov" -> 0 < d && d <= 30
+    | _ -> invalid_arg "m"
