03fc5bdb2d0800c055a106b3e58abdadd2036d47
James Chen <chenhao540@gmail.com>
2024-07-25 12:38:51 -0700

    bst impl

--- exercises511.ml
+++ exercises511.ml
@@ -1,34 +1,35 @@
-(* Exercise: complex synonym *)
+(* Exercise: complex synonym  *)
+
 module type ComplexSig = sig
-  type t
+  type t = float * float
   val zero : t
   val add : t -> t -> t
 end
 
 (* Exercise: complex encapsulation *)
-module Complex : ComplexSig = struct
-  type t = float * float
-  let zero = (0., 0.)
-  let add (r1, i1) (r2, i2) = r1 +. r2, i1 +. i2
-end
+(* remove zero from the structure
 
-(* remove zero from the structure: mismatched signature *)
+no identity element
 
-(* remove add from the signature: nothing happens *)
+remove add from the signature
 
-(* change zero in the structure to let zero = 0, 0: mismatched types *)
+no operation on the type float * float
+
+change zero in the structure to let zero = 0, 0  
+
+mismatched type with float * float and int * int *)
 
 (* Exercise: big list queue *)
 (** Creates a ListQueue filled with [n] elements. *)
-module type ListQueue = sig
-  val empty : 'a list
-  val enqueue : 'a -> 'a list -> 'a list
-end
-
 module ListQueue = struct
   let empty = []
-  let enqueue n q = q @ [n]
+
+  let enqueue (n: int) (q: 'a list) = 
+    match n with
+    | 0 -> q
+    | _ -> q @ [n]
 end
+
 
 let fill_listqueue n =
   let rec loop n q =
@@ -36,18 +37,17 @@ let fill_listqueue n =
     else loop (n - 1) (ListQueue.enqueue n q) in
   loop n ListQueue.empty
 
-(* 100000 -> signaficant delay; 10000 -> noticible delay *)
-
-(* Exercise: big batched queue *)
-module type BatchedQueue = sig
-  val empty : 'a list
-  val enqueue : 'a -> 'a list -> 'a list
-end
-
+(* Exercise: queue efficiency *)
 module BatchedQueue = struct
   let empty = []
-  let enqueue n q = n :: q
+
+  let enqueue n q = 
+    match n with 
+    | 0 -> q
+    | 1 -> List.rev (0 :: q)
+    | _ -> n :: q
 end
+
 
 let fill_batchedqueue n =
   let rec loop n q =
@@ -55,40 +55,48 @@ let fill_batchedqueue n =
     else loop (n - 1) (BatchedQueue.enqueue n q) in
   loop n BatchedQueue.empty
 
-(* faster than list queue *)
-
 (* Exercise: queue efficiency *)
-(* for every n, it will have to traverse from begining of the list to get to the end, then append new n to the end, so the complexity is n * n^2,*)
+(* listqueue: for each append operation, memory address locator has 
+  to traverse through the list to get to the end then apply append operation, which results in O(n^2) *)
 
-(* each enqueue operation is O(1), and there are n enqueue operations, so n * O(1) = O(n) *)
+(* bachedqueue: for each append operation, append operator is O(1) and List.rev is O(1), 
+  according to doc so overall complexity is O(1) *)
 
 (* Exercise: binary search tree map *)
 module type Map = sig
   type ('k, 'v) t
-  val empty: ('k, 'v) t
-  val insert: 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
-  val lookup: 'k -> ('k, 'v) t -> 'v
+  val empty  : ('k, 'v) t
+  val insert : 'k -> 'v -> ('k,'v) t -> ('k,'v) t
+  val lookup  : 'k -> ('k,'v) t -> 'v
 end
 
 module BstMap: Map = struct
-  type 'a bst = None | Node of 'a bst * 'a bst * 'a
+  type 'a tree = 
+  | None
+  | Node of 'a * 'a tree * 'a tree
 
-  type ('k, 'v) t = ('k, 'v) bst
-  
+  type ('k, 'v) t = ('k * 'v) tree
+
   let empty = None
 
-  let rec insert new_k new_v bst = 
-    match bst with 
-    | None -> Node (None, None, (k, v))
-    | (left, right, (k, v)) -> 
-      if new_k > k then insert new_k new_v right
-      else insert new_k new_v left
+  let rec insert k v t = 
+    match t with 
+    | None -> Node((k, v), None, None)
+    | Node ((k',v'), l, r) -> 
+      match k with 
+      | _ when k = k' -> Node((k, v), l, r)
+      | _ when k < k' -> Node((k', v'), insert k v l, r)
+      | _ when k > k' -> Node((k', v'), l, insert k v r)
+      [@@ocaml.warning "-8"]
 
-  let rec lookup targetKey bst = 
-    match bst with
-    | None -> failwith "not found"
-    | (_, _, (k, v)) when key = targetKey -> v
-    | (left, _, (k, v)) when targetKey < k -> lookup targetKey left
-    | (_, right, (k, v)) when targetKey > k -> lookup targetKey left
+  let rec lookup k t = 
+    match t with 
+    | None -> failwith "key not found"
+    | Node ((k',v'), l, r) -> 
+      match k with 
+      | _ when k = k' -> v'
+      | _ when k < k' -> lookup k l
+      | _ when k > k' -> lookup k r
+      [@@ocaml.warning "-8"]
 
 end
